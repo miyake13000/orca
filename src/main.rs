6e11fbbc75d532ba18cd45c4006d9f94c543510a
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-11-30 16:57:34 +0900

    Modified to use nix for syscall (WIP)

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 // This program is created by nomlab in Okayama University
 // https://github.com/miyake13000/crca
-
-//use fork::{daemon, Fork};
+extern crate pentry;
+extern crate nix;
+use nix::sched::*;
+//use std::process::Command;
 //use std::process::{Command, Child, Stdio};
-use std::process::Command;
-//use std::os::unix::io::{AsRawFd, FromRawFd};
 
 fn main() {
-    println!("starting fork...");
-    let output = Command::new("sleep")
-        .arg("10")
-        .output()
-        .expect("faisled to execuse process");
-    println!("done.");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    print_process_info();
+    const STACK_SIZE: usize = 1024 * 1024;
+    let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
+
+    let cb = Box::new(|| child());
+
+    let clone_flags: CloneFlags = CloneFlags::CLONE_NEWUTS ||
+                                  CloneFlags::CLONE_NEWIPC ||
+                                  CloneFlags::CLONE_NEWPID ||
+                                  CloneFlags::CLONE_NEWNET ||
+                                  CloneFlags::CLONE_NEWNS;
+
+    let p = clone(cb, stack, clone_flags, None);
+
+    let p = match p {
+        Ok(p) => p,
+        Err(err) => panic!(err),
+    };
+}
+
+fn child() -> isize {
+    print_process_info();
+    return 0;
+}
+
+fn print_process_info() {
+    if let Ok(ps) = pentry::current() {
+        println!("{:?}", ps);
+    };
 }
