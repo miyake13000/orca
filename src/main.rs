e721ca339dc67c4b35bbf0ed934f271b0e5f6db2
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-11-30 14:51:35 +0900

    Add function of CLI

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,50 @@
 //use std::process::{Command, Child, Stdio};
 use std::process::Command;
 //use std::os::unix::io::{AsRawFd, FromRawFd};
+#[macro_use]
+extern crate clap;
+use clap::{App, Arg, SubCommand};
 
 fn main() {
-    println!("starting fork...");
-    let output = Command::new("sleep")
-        .arg("10")
-        .output()
-        .expect("faisled to execuse process");
-    println!("done.");
-    println!("status: {}", output.status);
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    let app = App::new(crate_name!())
+        .version(crate_version!())
+        .author(crate_authors!())
+        .about(crate_description!())
+        .arg(Arg::with_name("pa")
+            .help("positional argument")
+            .required(true)
+        )
+        .arg(Arg::with_name("flg")
+            .help("flag")
+            .short("f")
+            .long("flag")
+        )
+        .arg(Arg::with_name("opt")
+            .help("option")
+            .short("o")
+            .long("option")
+            .takes_value(true)
+        )
+        .subcommand(SubCommand::with_name("sub")
+            .about("suncommand")
+            .arg(Arg::with_name("subflg")
+                .help("sub flag")
+                .short("f")
+                .long("flag")
+            )
+        );
+
+    let matches = app.get_matches();
+
+    if let Some(o) = matches.value_of("pa") {
+        println!("Value for pa: {}", o);
+    }
+    if let Some(o) = matches.value_of("opt") {
+        println!("Value for opt: {}", o);
+    }
+    println!("flg is {}", if matches.is_present("flg") {"true"} else {"false"});
+    if let Some(ref matches) = matches.subcommand_matches("sub") {
+        println!("uysed sub");
+        println!("subflg is {}", if matches.is_present("subflg") {"true"} else {"false"});
+    }
 }
