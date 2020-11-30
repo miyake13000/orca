301544c7308fa16821e6daf22b1a2a0c6342e704
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-11-30 14:03:37 +0900

    modified main.rs to fork 'sleep'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 // This program is created by nomlab in Okayama University
 // https://github.com/miyake13000/crca
 
+//use fork::{daemon, Fork};
+//use std::process::{Command, Child, Stdio};
+use std::process::Command;
+//use std::os::unix::io::{AsRawFd, FromRawFd};
+
 fn main() {
-    println!("Hello, world!");
+    println!("starting fork...");
+    let output = Command::new("sleep")
+        .arg("10")
+        .output()
+        .expect("faisled to execuse process");
+    println!("done.");
+    println!("status: {}", output.status);
+    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
 }
