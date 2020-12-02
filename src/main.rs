bb97d14a0aa7520a109a5b22770e1c3245569f8f
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-12-02 14:19:55 +0900

    Modified to isolate namespace and exec process

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@
 extern crate pentry;
 extern crate nix;
 use nix::sched::*;
+use nix::unistd;
+use std::ffi::CStr;
 
 fn main() {
     print_process_info();
@@ -14,11 +16,11 @@ fn main() {
 
     let mut clone_flags = CloneFlags::empty();
     clone_flags.insert(CloneFlags::CLONE_NEWUSER);
-//    clone_flags.insert(CloneFlags::CLONE_NEWUTS);
-//    clone_flags.insert(CloneFlags::CLONE_NEWIPC);
-//    clone_flags.insert(CloneFlags::CLONE_NEWPID);
-//    clone_flags.insert(CloneFlags::CLONE_NEWNET);
-//    clone_flags.insert(CloneFlags::CLONE_NEWNS);
+    clone_flags.insert(CloneFlags::CLONE_NEWUTS);
+    clone_flags.insert(CloneFlags::CLONE_NEWIPC);
+    clone_flags.insert(CloneFlags::CLONE_NEWPID);
+    clone_flags.insert(CloneFlags::CLONE_NEWNET);
+    clone_flags.insert(CloneFlags::CLONE_NEWNS);
 
     let p = clone(cb, stack, clone_flags, None);
     match p {
@@ -30,27 +32,15 @@ fn main() {
 fn child() -> isize {
     print_process_info();
 
-    const STACK_SIZE: usize = 1024 * 1024;
-    let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
-    let cb = Box::new(|| child2());
+    let path = CStr::from_bytes_with_nul(b"/bin/ls\0").unwrap();
+    let arg = CStr::from_bytes_with_nul(b"-al\0").unwrap();
 
-    let mut clone_flags = CloneFlags::empty();
-    clone_flags.insert(CloneFlags::CLONE_NEWUTS);
-    clone_flags.insert(CloneFlags::CLONE_NEWIPC);
-    clone_flags.insert(CloneFlags::CLONE_NEWPID);
-    clone_flags.insert(CloneFlags::CLONE_NEWNET);
-    clone_flags.insert(CloneFlags::CLONE_NEWNS);
+    let res = unistd::execv(path, &[arg]);
+    match res {
+        Ok(_ok) => println!("Success exec"),
+        Err(_err) => println!("failed to exec"),
+    }
 
-    let p = clone(cb, stack, clone_flags, None);
-    match p {
-        Ok(_pid)  => println!("child_success"),
-        Err(_err) => println!("child_failes to clone process"),
-    };
-    return 0;
-}
-
-fn child2() -> isize {
-    print_process_info();
     return 0;
 }
 
