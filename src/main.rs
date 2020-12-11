ca47f0f19ad0b8232294f53b6fefd3ebd71e7a8f
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-12-11 17:58:36 +0900

    Parse json of curl response

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
 // This program is created by nomlab in Okayama University
 // https://github.com/miyake13000/crca
 
+//extern crate serde;
+extern crate serde_json;
+
 use std::io::{stdout, Write};
 use curl::easy::Easy;
+use std::collections::BTreeMap;
 
 fn main() {
-    let mut easy = Easy::new();
-    easy.url("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/alpine:pull").unwrap();
-    easy.write_function(|data| {
-        stdout().write_all(data).unwrap();
+    let mut res = Vec::new();
+    let mut handle = Easy::new();
+    handle.url("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/alpine:pull").unwrap();
+    let mut transfer = handle.transfer();
+    transfer.write_function(|data| {
+        res.extend_from_slice(data);
         Ok(data.len())
     }).unwrap();
-    easy.perform().unwrap();
+    transfer.perform().unwrap();
 
-    println!("{}",easy.response_code().unwrap());
+    let res_string = String::from_utf8_lossy(&res);
+    let res_json: BTreeMap<String, String> = serde_json::from_str(&res_string).unwrap();
+    let token = res_json.get("token").unwrap();
+
+    println!("{}", token);
 }
