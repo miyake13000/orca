5001865a12db575616c6a2d725ab1bf803259b5b
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-12-11 17:20:34 +0900

    Add curl

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
 // This program is created by nomlab in Okayama University
 // https://github.com/miyake13000/crca
 
+use std::io::{stdout, Write};
+use curl::easy::Easy;
+
 fn main() {
-    println!("Hello, world!");
+    let mut easy = Easy::new();
+    easy.url("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/alpine:pull").unwrap();
+    easy.write_function(|data| {
+        stdout().write_all(data).unwrap();
+        Ok(data.len())
+    }).unwrap();
+    easy.perform().unwrap();
+
+    println!("{}",easy.response_code().unwrap());
 }
