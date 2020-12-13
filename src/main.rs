09786077228eb4a52dbd7821f19820e29203d778
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2020-12-13 21:15:05 +0900

    Modified to split image into struct

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use dirs::home_dir;
 
 #[derive(Serialize, Deserialize)]
 #[allow(non_snake_case)]
-struct CurlRes1 {
+struct Res1 {
     token: String,
     access_token: String,
     expires_in: u32,
@@ -31,76 +31,93 @@ struct Config {
 
 #[derive(Serialize, Deserialize)]
 #[allow(non_snake_case)]
-struct CurlRes2 {
+struct Res2 {
     schemaVersion: usize,
     mediaType: String,
     config: Config,
     layers: Vec<Config>,
 }
 
-fn main() {
-    let dest_name = "alpine";
-    let dest_ver = "3.10";
+pub struct Image {
+    dest_name: String,
+    dest_tag:  String,
+    path: PathBuf,
+}
 
-    let mut path = PathBuf::new();
-    let home_dir = home_dir().unwrap();
-    path.push(home_dir);
-    path.push(".local/orca/containers");
-    path.push(dest_name);
-    path.push(dest_ver);
-    println!("path:{}", path.to_str().unwrap());
+impl Image {
+    pub fn new(name: String, tag: String) -> Image {
+        let mut path = PathBuf::new();
+        let home_dir = home_dir().unwrap();
+        path.push(home_dir);
+        path.push(".local/orca/containers");
+        path.push(&name);
+        path.push(&tag);
 
-    if Path::new(path.as_path()).exists() {
-        println!("Container image has already existed");
-        return;
+        Image{dest_name: name, dest_tag: tag, path: path}
     }
 
-    let url = format!("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{}:pull", dest_name);
-    let client = reqwest::blocking::Client::new();
-    let resp = client.get(&url)
-        .send()
-        .unwrap()
-        .text()
-        .unwrap();
-    let res_json: CurlRes1 = serde_json::from_str(&resp).unwrap();
-    let token = &res_json.token;
-    println!("Found image");
+    pub fn exist(&self) -> bool {
+        Path::new(self.path.as_path()).exists()
+    }
 
-    let url = format!("https://registry-1.docker.io/v2/library/{}/manifests/{}", dest_name, dest_ver);
-    let client = reqwest::blocking::Client::new();
-    let resp = client.get(&url)
-        .header(reqwest::header::ACCEPT, "application/vnd.docker.distribution.manifest.v2+json")
-        .bearer_auth(token)
-        .send()
-        .unwrap()
-        .text()
-        .unwrap();
-    let res_json: CurlRes2 = serde_json::from_str(&resp).unwrap();
-    let layer_id = &res_json.layers[0].digest;
+    pub fn get(&self) -> Result<(), ()> {
+        let url = format!("https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{}:pull", self.dest_name);
+        let client = reqwest::blocking::Client::new();
+        let resp = client.get(&url)
+            .send()
+            .unwrap()
+            .text()
+            .unwrap();
+        let res_json: Res1 = serde_json::from_str(&resp).unwrap();
+        let token = &res_json.token;
 
-    println!("Downloading image...");
-    let url = format!("https://registry-1.docker.io/v2/library/{}/blobs/{}", dest_name, layer_id);
-    let client = reqwest::blocking::Client::new();
-    let mut resp = client.get(&url)
-        .bearer_auth(token)
-        .send()
-        .unwrap();
+        let url = format!("https://registry-1.docker.io/v2/library/{}/manifests/{}", self.dest_name, self.dest_tag);
+        let client = reqwest::blocking::Client::new();
+        let resp = client.get(&url)
+            .header(reqwest::header::ACCEPT, "application/vnd.docker.distribution.manifest.v2+json")
+            .bearer_auth(token)
+            .send()
+            .unwrap()
+            .text()
+            .unwrap();
+        let res_json: Res2 = serde_json::from_str(&resp).unwrap();
+        let layer_id = &res_json.layers[0].digest;
 
-    create_dir_all(path.as_path()).expect("create directory");
-    path.push("image.tar.gz");
-    println!("path:{}", path.to_str().unwrap());
-    let mut file = File::create(path.as_path()).expect("file create");
-    io::copy(&mut resp, &mut file).expect("copy");
+        let url = format!("https://registry-1.docker.io/v2/library/{}/blobs/{}", self.dest_name, layer_id);
+        let client = reqwest::blocking::Client::new();
+        let mut resp = client.get(&url)
+            .bearer_auth(token)
+            .send()
+            .unwrap();
 
-    println!("Extracting image");
-    let tar_gz = File::open(path.as_path()).expect("file open");
-    let tar = GzDecoder::new(tar_gz);
-    let mut archive = Archive::new(tar);
-    path.pop();
-    path.push("rootfs");
-    println!("path:{}", path.to_str().unwrap());
-    archive.unpack(path.as_path()).expect("unpack");
+        create_dir_all(self.path.as_path()).expect("create directory");
+        let path = format!("{}/image.tar.gz", self.path.to_str().unwrap());
+        let mut file = File::create(path).expect("file create");
+        io::copy(&mut resp, &mut file).expect("copy");
+        Ok(())
+    }
 
-    return;
+    pub fn extract(&self) -> Result<(), ()> {
+        let path = format!("{}/image.tar.gz", self.path.to_str().unwrap());
+        let tar_gz = File::open(path).expect("file open");
+        let tar = GzDecoder::new(tar_gz);
+        let mut archive = Archive::new(tar);
+        let path = format!("{}/rootfs", self.path.to_str().unwrap());
+        archive.unpack(path).expect("unpack");
+        Ok(())
+    }
+}
+
+fn main() {
+    let image = Image::new(String::from("debian"), String::from("latest"));
+    if  image.exist() {
+        println!("image has exist");
+    } else {
+        println!("Downloading container image...");
+        image.get().unwrap();
+        println!("Extracting container image...");
+        image.extract().unwrap();
+        println!("done");
+    }
 }
 
