74172d68b98b272da69019575e210b46af471b57
miyake13000 <pnpm01sm@s.okayama-u.ac.jp>
2021-08-23 02:59:05 +0900

    Updadte scripts

--- build_static_openssl.sh
+++ build_static_openssl.sh
@@ -6,17 +6,19 @@ ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/x86_64-linux-musl/asm
 ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/asm-generic
 ln -s /usr/include/linux /usr/include/x86_64-linux-musl/linux
 
-mkdir /musl
+mkdir /tmp/musl
 
 wget https://github.com/openssl/openssl/archive/OpenSSL_1_1_1f.tar.gz
 tar zxvf OpenSSL_1_1_1f.tar.gz
 cd openssl-OpenSSL_1_1_1f/
 
-CC="musl-gcc -fPIE -pie" ./Configure no-shared no-async --prefix=/musl --openssldir=/musl/ssl linux-x86_64
+CC="musl-gcc -fPIE -pie" ./Configure no-shared no-async --prefix=/tmp/musl --openssldir=/tmp/musl/ssl linux-x86_64
 
 make depend
 make -j$(nproc)
 make install
+
+cd ../
 
 rm OpenSSL_1_1_1f.tar.gz
 rm -r openssl-OpenSSL_1_1_1f
--- create_static_binary.sh
+++ create_static_binary.sh
@@ -4,5 +4,5 @@
 
 export PKG_CONFIG_ALLOW_CROSS=1
 export OPENSSL_STATIC=true
-export OPENSSL_DIR=/musl
-cargo build --target x86_64-unknown-linux-musl
+export OPENSSL_DIR=/tmp/musl
+cargo build --release --target x86_64-unknown-linux-musl
