3c053138d0e7772273cb3b2cfa908557d1457b5e
miyake13000 <miyake2020@s.okayama-u.ac.jp>
2022-06-15 00:08:56 +0900

    Add: host_image module for treat host image container

--- src/image.rs
+++ src/image.rs
@@ -1,9 +1,11 @@
 pub mod guest_image;
+pub mod host_image;
 
 use crate::mount::{FileType, Mount, MountFlags};
 use anyhow::{Context, Result};
 use guest_image::GuestImage;
-use std::path::PathBuf;
+use host_image::HostImage;
+use std::path::{Path, PathBuf};
 
 pub trait ContainerImage {
     fn mount(&self) -> Result<()>;
@@ -35,5 +37,31 @@ impl ContainerImage for GuestImage {
 
     fn need_userns(&self) -> bool {
         true
+    }
+}
+
+impl ContainerImage for HostImage {
+    fn mount(&self) -> Result<()> {
+        Mount::new(&self.container_path(), FileType::Dir)
+            .flags(MountFlags::MS_PRIVATE)
+            .flags(MountFlags::MS_REC)
+            .data(format!(
+                "lowerdir=/,upperdir={},workdir={}",
+                &self.ovr_upperdir.display(),
+                &self.ovr_workdir.display()
+            ))
+            .mount()
+    }
+
+    fn name(&self) -> String {
+        self.container_name()
+    }
+
+    fn root_path(&self) -> PathBuf {
+        self.container_path()
+    }
+
+    fn need_userns(&self) -> bool {
+        false
     }
 }
---
+++ src/image/host_image.rs
@@ -0,0 +1,62 @@
+use anyhow::{Context, Result};
+use rm_rf::remove;
+use std::fs::create_dir_all;
+use std::path::{Path, PathBuf};
+
+const ROOTFS_NAME: &str = "rootfs";
+const UPPERDIR_NAME: &str = "upper";
+const WORKDIR_NAME: &str = "work";
+const IMAGE_NAME: &str = "host_image";
+
+pub struct HostImage {
+    pub ovr_upperdir: PathBuf,
+    pub ovr_workdir: PathBuf,
+    container_path: PathBuf,
+    container_name: String,
+}
+
+impl HostImage {
+    pub fn new<S, T>(rootfs_prefix: S, container_name: T) -> Self
+    where
+        S: AsRef<Path>,
+        T: ToString,
+    {
+        let rootfs_prefix = rootfs_prefix.as_ref().to_path_buf();
+        let container_name = container_name.to_string();
+        let container_root = rootfs_prefix.join(IMAGE_NAME).join(&container_name);
+        let container_path = container_root.join(ROOTFS_NAME);
+        let ovr_upperdir = container_root.join(UPPERDIR_NAME);
+        let ovr_workdir = container_root.join(WORKDIR_NAME);
+
+        Self {
+            ovr_upperdir,
+            ovr_workdir,
+            container_path,
+            container_name,
+        }
+    }
+
+    pub fn create(&self) -> Result<()> {
+        create_dir_all(&self.ovr_upperdir)?;
+        create_dir_all(&self.ovr_workdir)?;
+        create_dir_all(&self.container_path)?;
+        Ok(())
+    }
+
+    pub fn container_path(&self) -> PathBuf {
+        self.container_path.clone()
+    }
+
+    pub fn exists_container(&self) -> bool {
+        self.ovr_upperdir.exists()
+    }
+
+    pub fn remove_container(&self) -> Result<()> {
+        remove(&self.ovr_upperdir)?;
+        Ok(())
+    }
+
+    pub fn container_name(&self) -> String {
+        self.container_name.clone()
+    }
+}
